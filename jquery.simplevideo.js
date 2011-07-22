09407bba7de0bef1d98d12496d45882b94ac651b
Aaron Johnson <aaron@aaronjohnson.me>
2011-07-22 13:10:57 +0000

    Added Modernizr support and fallback to hard coded capability checks

--- jquery.simplevideo.js
+++ jquery.simplevideo.js
@@ -1,4 +1,34 @@
 (function($){
+var _bool;
+var capabilities = function(){
+	if (window.Modernizr){
+		return window.Modernizr.video;
+	}
+	if (_bool != undefined){
+		return _bool;
+	}	
+	//Excerpt of Modernizr if you don't have it included
+	var elem = document.createElement('video'),
+       	 bool = false;
+	            
+        // IE9 Running on Windows Server SKU can cause an exception to be thrown, bug #224
+        try {
+        	if ( bool = !!elem.canPlayType ) {
+                	bool = new Boolean(bool);
+                	bool.ogg  = elem.canPlayType('video/ogg; codecs="theora"');
+
+                	// Workaround required for IE9, which doesn't report video support without audio codec specified.
+                	//   bug 599718 @ msft connect
+                	var h264 = 'video/mp4; codecs="avc1.42E01E';
+                	bool.h264 = elem.canPlayType(h264 + '"') || elem.canPlayType(h264 + ', mp4a.40.2"');
+
+                	bool.webm = elem.canPlayType('video/webm; codecs="vp8, vorbis"');
+            	}
+	            
+        } catch(e) { }
+	_bool = bool;
+        return bool;
+}
 $.fn.extend({
 	var simplevideo = function(o){
 		var defaults = {
@@ -66,15 +96,16 @@ $.fn.extend({
 			}
 			var src = $('<source />');
 			var compat = false;
-			if (vidElement.canPlayType('video/webm') && settings.webm){
+
+			if (capabilities().webm && settings.webm){
 				$src.attr('src', settings.webm);
 				$src.attr('type', 'video/webm');
 				compat = true;
-			} else if (vidElement.canPlayType('video/mp4') && settings.mp4){
+			} else if (capabilities().h264 && settings.mp4){
 				$src.attr('src', settings.mp4);
 				$src.attr('type', 'video/mp4');
 				compat = true;
-			} else if (vidElement.canPlayType('video/ogg') && settings.ogv){
+			} else if (capabilities().ogg && settings.ogv){
 				$src.attr('src', settings.ogv);
 				$src.attr('type', 'video/ogg');
 				compat = true;
